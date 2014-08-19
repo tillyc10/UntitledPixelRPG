6db12d0d74b87d602e87d4d3ce1057f643252155
Corey Tilly <dezigngamez@gmail.com>
2014-08-18 21:05:35 -0400

    Initial Commit
    Very basic project set up in Unity

---
+++ UntitledRPG/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float walkSpeed;
+
+	void Start()
+	{
+
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0.0f);
+
+		rigidbody2D.velocity = movement * walkSpeed;
+		rigidbody2D.rotation = 0;
+	}
+
+}
