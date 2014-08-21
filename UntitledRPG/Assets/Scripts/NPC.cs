941882613d121c31233389b2cf5b51f3c76c1914
Scott Sanderson <Ireallylovehaylea@gmail.com>
2014-08-21 17:42:05 -0400

    NPC Interact/PC Movement
    Added a small shop dialogue to the NPC (currently nonfunctioning) and I
    also changed the players movement to be locked to the cardinal
    directions.  It still needs work however.

--- UntitledRPG/Assets/Scripts/NPC.cs
+++ UntitledRPG/Assets/Scripts/NPC.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class NPC : MonoBehaviour {
 
 	private bool bTalk = false;
+	private bool bTalkShop = false;
 	private int bAdvance;
+	private int bShop;
 	private float counter;
 
 	// Use this for initialization
@@ -18,13 +20,64 @@ public class NPC : MonoBehaviour {
 		if ( other.tag == "Player" )
 		{
 			bTalk = true;
+			bTalkShop = true;
 		}
 	}
 
 	void OnGUI()
 	{
+		if (bTalkShop) 
+		{
+			if (GUI.Button (new Rect (Screen.width / 2 - 50, (Screen.height / 5) * 3, 100, 50), "Shop"))
+			{
+				bShop++;
+				bTalkShop = false;
+				bTalk = false;
+				counter = 0;
+			} 
+			else
+				counter += Time.deltaTime;
+
+		}
+		if(bShop == 1)
+		{
+			if( GUI.Button( new Rect( Screen.width/2 - 50, (Screen.height/5) * 3, 100, 50 ), "Buy"))
+			{
+				bShop = 2;
+			}
+			else					
+				counter += Time.deltaTime;
+
+			if( GUI.Button( new Rect( Screen.width/2 - 50, (Screen.height/4) * 3, 100, 50 ), "Sell"))
+			{
+				bShop = 3;
+			}				
+			else
+				counter += Time.deltaTime;
+		}
+		if(bShop == 2) 
+		{
+			if( GUI.Button( new Rect( Screen.width/2 - 125, (Screen.height/5) * 3, 250, 50 ), "Sorry you don't have any money"))
+			{
+				bShop = 0;
+			}
+		}
+		if (bShop == 3) 
+		{
+			if( GUI.Button( new Rect( Screen.width/2 - 125, (Screen.height/5) * 3, 260, 50 ), "Sorry you don't have an Inventory just yet"))
+			{
+				bShop = 0;
+			}
+		}
+		if ( counter >= 5 )
+		{
+			bShop = 0;
+			counter = 0;
+		}
+
 		if ( bTalk )
 		{
+
 			if( GUI.Button( new Rect( Screen.width/2 - 50, (Screen.height/4) * 3, 100, 50 ), "Hello!"))
 			{
 				Debug.Log ("Hahahahaha");
@@ -38,6 +91,7 @@ public class NPC : MonoBehaviour {
 			if ( counter >= 5 )
 			{
 				bTalk = false;
+				bTalkShop = false;
 				counter = 0;
 			}
 		}
--- UntitledRPG/Assets/Scripts/PlayerMovement.cs
+++ UntitledRPG/Assets/Scripts/PlayerMovement.cs
@@ -14,8 +14,17 @@ public class PlayerMovement : MonoBehaviour {
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
-
+		if (moveHorizontal > 0 || moveHorizontal < 0) 
+		{
+			moveVertical = 0.0f;
+		} 
+		if (moveVertical > 0 || moveVertical < 0) 
+		{
+			moveHorizontal = 0.0f;
+		}
 		Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0.0f);
+
+			
 
 		rigidbody2D.velocity = movement * walkSpeed;
 		rigidbody2D.rotation = 0;
