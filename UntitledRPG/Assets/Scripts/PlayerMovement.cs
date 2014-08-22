562d387a249b1db2b3309ad08aa9be2d7ccabead
Corey Tilly <dezigngamez@gmail.com>
2014-08-21 22:12:46 -0400

    Fixed movement bug

--- UntitledRPG/Assets/Scripts/PlayerMovement.cs
+++ UntitledRPG/Assets/Scripts/PlayerMovement.cs
@@ -5,29 +5,104 @@ public class PlayerMovement : MonoBehaviour {
 
 	public float walkSpeed;
 
+	private float curHeld;
+	private float recentPress;
+
 	void Start()
 	{
 
 	}
 
-	void FixedUpdate()
+	// Returns value of true to move horizontal and false to move vertical
+	private bool MoveWhichWay (float moveHorizontal, float moveVertical)
+	{
+		if ( moveVertical == 0 && moveHorizontal == 0 )
+		{
+			curHeld = 0;
+			recentPress = 0;
+			return false;
+		}
+		else if ( moveVertical == 0 && moveHorizontal != 0 )
+		{
+			curHeld = 1;
+			recentPress = 1;
+			return true;
+		}
+		else if ( moveVertical != 0 && moveHorizontal == 0 )
+		{
+			curHeld = 2;
+			recentPress = 2;
+			return false;
+		}
+		else if ( moveVertical != 0 && moveHorizontal != 0 )
+		{
+			if ( curHeld == 1 && recentPress == 1 )
+			{
+				return false;
+			}
+			else if ( curHeld == 1 && recentPress == 2 )
+			{
+				curHeld = 2;
+				return true;
+			}
+			else if ( curHeld == 2 && recentPress == 1 )
+			{
+				curHeld = 1;
+				return false;
+			}
+			else if ( curHeld == 2 && recentPress == 2 )
+			{
+				return true;
+			}
+
+			return false;
+		}
+
+		return false;
+	}
+
+	void MovePlayer ()
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
+
+		if ( MoveWhichWay( moveHorizontal, moveVertical ) )
+		{
+			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, 0.0f);
+			rigidbody2D.velocity = movement * walkSpeed;
+		}
+		else
+		{
+			Vector3 movement = new Vector3 (0.0f, moveVertical, 0.0f);
+			rigidbody2D.velocity = movement * walkSpeed;
+		}
+
+		rigidbody2D.rotation = 0;
+	}
+
+	void FixedUpdate()
+	{
+		MovePlayer ();
+
+		/*float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
 		if (moveHorizontal > 0 || moveHorizontal < 0) 
 		{
 			moveVertical = 0.0f;
-		} 
+		}
+
 		if (moveVertical > 0 || moveVertical < 0) 
 		{
 			moveHorizontal = 0.0f;
 		}
+
 		Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0.0f);
 
 			
 
 		rigidbody2D.velocity = movement * walkSpeed;
-		rigidbody2D.rotation = 0;
+		rigidbody2D.rotation = 0;*/
 	}
 
 }
