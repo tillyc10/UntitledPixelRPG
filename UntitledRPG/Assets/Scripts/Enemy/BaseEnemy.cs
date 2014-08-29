0f786ba025f2a97be63b5d42c4dfa240eff79be5
Jaybils <jayweldo@gmail.com>
2014-08-28 21:06:01 -0500

    added multiple attack/damage slots

--- UntitledRPG/Assets/Scripts/Enemy/BaseEnemy.cs
+++ UntitledRPG/Assets/Scripts/Enemy/BaseEnemy.cs
@@ -7,7 +7,15 @@ public class BaseEnemy : MonoBehaviour
 	public string enemyDescription;
 	public int enemyLevel;
 	public int enemyHealth;
-	public int enemyDamage;
+
+	// just pair attack 1 with damage 1
+	public string enemyAttack1;
+	public string enemyAttack2;
+	public string enemyAttack3;
+	public int enemyDamage1;
+	public int enemyDamage2;
+	public int enemyDamage3;
+
 	public int enemyDefense;
 	public int enemyGold;
 	public string enemyType;
