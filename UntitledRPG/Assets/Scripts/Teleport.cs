70b17b809f0357f88641e07e0950edeb97a36b7d
Scott Sanderson <Ireallylovehaylea@gmail.com>
2014-09-02 21:26:45 -0400

    more changes

--- UntitledRPG/Assets/Scripts/Teleport.cs
+++ UntitledRPG/Assets/Scripts/Teleport.cs
@@ -4,26 +4,13 @@ using System.Collections;
 public class Teleport : MonoBehaviour {
 
 	// Use this for initialization
-	public bool dungeon = false;
-	public bool village = true;
+	public string teleportTarget;
 	void Start () {
 	
 	}
 	void OnTriggerEnter2D(Collider2D other)
 	{
-		print ( "Collided with" );
-		if (village) 
-		{
-			Application.LoadLevel ("DungeonTest");
-			village = false;
-			dungeon = true;
-		}
-		else if (dungeon)
-		{
-			Application.LoadLevel ("Test");
-			village = true;
-			dungeon = false;
-		}
+		Application.LoadLevel (teleportTarget);
 	}
 	
 	// Update is called once per frame
