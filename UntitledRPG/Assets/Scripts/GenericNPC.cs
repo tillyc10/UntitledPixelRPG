c6abb8e3a83ed95090c93313ec2f2836168cfb60
Corey Tilly <dezigngamez@gmail.com>
2014-09-13 14:20:16 -0400

    Added some npc's to InventoryTest
    Shows some additions to GenericNPC script by talking to
    blacksmith(berzerker sprite) and oracle(blue circle).

--- UntitledRPG/Assets/Scripts/GenericNPC.cs
+++ UntitledRPG/Assets/Scripts/GenericNPC.cs
@@ -12,7 +12,7 @@ public class GenericNPC : MonoBehaviour {
 	public int introWidth;
 	public int introHeight;
 
-	private Rect npcWindowRect = new Rect((Screen.width * 0.5f) - 200, (Screen.height * 0.75f), 400, 100);
+	private Rect npcWindowRect = new Rect((Screen.width * 0.5f) - 200, (Screen.height * 0.75f), 400, 75);
 	private const int NPCWindowID = 0;
 
 	public string[] generalDialouges;
@@ -28,6 +28,12 @@ public class GenericNPC : MonoBehaviour {
 	private bool bIntro;
 	private bool bMet;
 
+	private bool bConversation;
+	private bool bQuestGiving;
+	private bool bShopping;
+
+	private int buttonsToDraw = 0;
+
 
 
 	// Use this for initialization
@@ -35,6 +41,27 @@ public class GenericNPC : MonoBehaviour {
 
 		player = GameObject.FindGameObjectWithTag("Player");
 		playerInventory = player.GetComponent<Inventory>();
+
+		if ( bShop && bQuestGiver )
+		{
+			npcWindowRect = new Rect((Screen.width * 0.5f) - 150, (Screen.height * 0.75f), 300, 75);
+			buttonsToDraw = 3;
+		}
+		else if ( bShop )
+		{
+			npcWindowRect = new Rect((Screen.width * 0.5f) - 100, (Screen.height * 0.75f), 200, 75);
+			buttonsToDraw = 2;
+		}
+		else if ( bQuestGiver )
+		{
+			npcWindowRect = new Rect((Screen.width * 0.5f) - 100, (Screen.height * 0.75f), 200, 75);
+			buttonsToDraw = 2;
+		}
+		else
+		{
+			npcWindowRect = new Rect((Screen.width * 0.5f) - 60, (Screen.height * 0.75f), 120, 75);
+			buttonsToDraw = 1;
+		}
 	
 	}
 
@@ -64,7 +91,7 @@ public class GenericNPC : MonoBehaviour {
 			if ( !bIntro && !bMet )
 			{
 				// Hit the "talk" button to begin conversation
-				if ( GUI.Button ( new Rect ( Screen.width * 0.5f, (Screen.height * 0.75f), 80, 40 ), "Talk" ) )
+				if ( GUI.Button ( new Rect ( Screen.width * 0.5f - 40, (Screen.height * 0.75f), 80, 40 ), "Talk" ) )
 				{
 					bIntro = true;
 				}
@@ -73,7 +100,7 @@ public class GenericNPC : MonoBehaviour {
 			if ( bIntro && !bMet )
 			{
 				// NPC introduction
-				if ( GUI.Button ( new Rect ( Screen.width * 0.5f, (Screen.height * 0.75f), introWidth, introHeight ), Introduction ) )
+				if ( GUI.Button ( new Rect ( (Screen.width * 0.5f) - (introWidth * 0.5f), (Screen.height * 0.75f), introWidth, introHeight ), Introduction ) )
 				{
 					bTalk = true;
 					bMet = true;
@@ -83,13 +110,63 @@ public class GenericNPC : MonoBehaviour {
 			if ( bTalk )
 			{
 				npcWindowRect = GUI.Window (NPCWindowID, npcWindowRect, NPCWindow, NPCName );
+
+				if ( bConversation )
+				{
+					// Begin Conversation
+				}
+
+				if ( bShopping )
+				{
+					// Begin Shopping
+				}
+
+				if ( bQuestGiving )
+				{
+					// Begin giving quests
+				}
 			}
 		}
 	}
 
 	public void NPCWindow( int ID )
 	{
+		if ( buttonsToDraw == 1 )
+		{
+			if ( GUI.Button ( new Rect ( npcWindowRect.width * 0.5f - 40, npcWindowRect.height * 0.5f - 15, 80, 40 ), "Talk" ) )
+				bConversation = true;
+		}
 
+		if ( buttonsToDraw == 2 && bShop )
+		{
+			if ( GUI.Button ( new Rect ( npcWindowRect.width * 0.25f - 40, npcWindowRect.height * 0.5f - 15, 80, 40 ), "Talk" ) )
+				bConversation = true;
+
+			if ( GUI.Button ( new Rect ( npcWindowRect.width * 0.75f - 40, npcWindowRect.height * 0.5f - 15, 80, 40 ), "Shop" ) )
+				bShopping = true;
+
+		}
+
+		if ( buttonsToDraw == 2 && bQuestGiver )
+		{
+			if ( GUI.Button ( new Rect ( npcWindowRect.width * 0.25f - 40, npcWindowRect.height * 0.5f - 15, 80, 40 ), "Talk" ) )
+				bConversation = true;
+
+			if ( GUI.Button ( new Rect ( npcWindowRect.width * 0.75f - 40, npcWindowRect.height * 0.5f - 15, 80, 40 ), "Quest" ) )
+				bQuestGiving = true;
+		}
+
+		if ( buttonsToDraw == 3 )
+		{
+			if ( GUI.Button ( new Rect ( npcWindowRect.width * 0.2f - 40, npcWindowRect.height * 0.5f - 15, 80, 40 ), "Talk" ) )
+				bConversation = true;
+
+			if ( GUI.Button ( new Rect ( npcWindowRect.width * 0.5f - 40, npcWindowRect.height * 0.5f - 15, 80, 40 ), "Shop" ) )
+				bShopping = true;
+
+			if ( GUI.Button ( new Rect ( npcWindowRect.width * 0.8f - 40, npcWindowRect.height * 0.5f - 15, 80, 40 ), "Quest" ) )
+				bQuestGiving = true;
+		}
 	}
 	
 	// Update is called once per frame
