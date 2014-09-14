0111f46ecf4f6738bfe158e49fc154d168453fd9
Corey Tilly <dezigngamez@gmail.com>
2014-09-14 11:05:12 -0400

    More updates to the newer npc script

--- UntitledRPG/Assets/Scripts/GenericNPC.cs
+++ UntitledRPG/Assets/Scripts/GenericNPC.cs
@@ -12,17 +12,41 @@ public class GenericNPC : MonoBehaviour {
 	public int introWidth;
 	public int introHeight;
 
+	public string[] generalDialouges;
+	public string[] shopDialouges;
+
+	public int numberOfQuests;
+	public string[] questDialouges;
+
+	//----------------------------------------------------------------------------------------------------------------------------
+	// Window Variables
 	private Rect npcWindowRect = new Rect((Screen.width * 0.5f) - 200, (Screen.height * 0.75f), 400, 75);
 	private const int NPCWindowID = 0;
 
-	public string[] generalDialouges;
-	public string[] shopDialouges;
-	public string[] questDialouges;
-	
+	private Rect shopWindowRect = new Rect((Screen.width * 0.75f) - 200, (Screen.height * 0.5f) - 160, 400, 320);
+	private const int ShopWindowID = 1;
+
+	private Rect questWindowRect = new Rect((Screen.width * 0.75f) - 100, (Screen.height * 0.5f) - 160, 200, 320);
+	private const int QuestWindowID = 2;
+
+	private Rect inventoryWindowRect = new Rect((Screen.width * 0.25f) - 200, (Screen.height * 0.5f) - 160, 400, 320);
+	private const int InventoryWindowID = 3;
+
+	private Rect descriptionWindowRect = new Rect((Screen.width * 0.75f) - 200, (Screen.height * 0.5f) - 160, 400, 320);
+	private const int DescriptionWindowID = 4;
+
+	private Rect sellWindowRect = new Rect((Screen.width * 0.5f) - 100, (Screen.height * 0.5f) - 90, 200, 180);
+	private const int SellWindowID = 5;
+
+
+	//----------------------------------------------------------------------------------------------------------------------------
 	// References
 	private Inventory playerInventory;
+	private Item itemList;
 	private GameObject player;
 
+	//----------------------------------------------------------------------------------------------------------------------------
+	// Private Bools
 	private bool inVicinity;
 	private bool bTalk;
 	private bool bIntro;
@@ -31,16 +55,21 @@ public class GenericNPC : MonoBehaviour {
 	private bool bConversation;
 	private bool bQuestGiving;
 	private bool bShopping;
+	private bool bSelling;
+	private bool bBuying;
 
+	//----------------------------------------------------------------------------------------------------------------------------
+	// Private Ints
 	private int buttonsToDraw = 0;
 
 
-
+	//----------------------------------------------------------------------------------------------------------------------------
 	// Use this for initialization
 	void Start () {
 
 		player = GameObject.FindGameObjectWithTag("Player");
 		playerInventory = player.GetComponent<Inventory>();
+		itemList = player.GetComponent<Item>();
 
 		if ( bShop && bQuestGiver )
 		{
@@ -65,6 +94,8 @@ public class GenericNPC : MonoBehaviour {
 	
 	}
 
+
+	//----------------------------------------------------------------------------------------------------------------------------
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		//print ( "Collided with" );
@@ -80,9 +111,14 @@ public class GenericNPC : MonoBehaviour {
 		if ( other.tag == "Player" )
 		{
 			inVicinity = false;
+			bShopping = false;
+			bQuestGiving = false;
+			bConversation = false;
 		}
 	}
 
+
+	//----------------------------------------------------------------------------------------------------------------------------
 	void OnGUI()
 	{
 		// We are close enough to begin conversation
@@ -119,56 +155,137 @@ public class GenericNPC : MonoBehaviour {
 				if ( bShopping )
 				{
 					// Begin Shopping
+					shopWindowRect = GUI.Window (ShopWindowID, shopWindowRect, ShopWindow, "Shop" );
 				}
 
 				if ( bQuestGiving )
 				{
 					// Begin giving quests
+					questWindowRect = GUI.Window (QuestWindowID, questWindowRect, QuestWindow, "Quest" );
 				}
 			}
 		}
 	}
 
+
+	//----------------------------------------------------------------------------------------------------------------------------
 	public void NPCWindow( int ID )
 	{
 		if ( buttonsToDraw == 1 )
 		{
 			if ( GUI.Button ( new Rect ( npcWindowRect.width * 0.5f - 40, npcWindowRect.height * 0.5f - 15, 80, 40 ), "Talk" ) )
+			{
+				bQuestGiving = false;
+				bShopping = false;
 				bConversation = true;
+			}
 		}
 
 		if ( buttonsToDraw == 2 && bShop )
 		{
 			if ( GUI.Button ( new Rect ( npcWindowRect.width * 0.25f - 40, npcWindowRect.height * 0.5f - 15, 80, 40 ), "Talk" ) )
+			{
+				bQuestGiving = false;
+				bShopping = false;
 				bConversation = true;
+			}
 
 			if ( GUI.Button ( new Rect ( npcWindowRect.width * 0.75f - 40, npcWindowRect.height * 0.5f - 15, 80, 40 ), "Shop" ) )
+			{
+				bConversation = false;
+				bQuestGiving = false;
 				bShopping = true;
+			}
 
 		}
 
 		if ( buttonsToDraw == 2 && bQuestGiver )
 		{
 			if ( GUI.Button ( new Rect ( npcWindowRect.width * 0.25f - 40, npcWindowRect.height * 0.5f - 15, 80, 40 ), "Talk" ) )
+			{
+				bQuestGiving = false;
+				bShopping = false;
 				bConversation = true;
+			}
 
 			if ( GUI.Button ( new Rect ( npcWindowRect.width * 0.75f - 40, npcWindowRect.height * 0.5f - 15, 80, 40 ), "Quest" ) )
+			{
+				bConversation = false;
+				bShopping = false;
 				bQuestGiving = true;
+			}
 		}
 
 		if ( buttonsToDraw == 3 )
 		{
 			if ( GUI.Button ( new Rect ( npcWindowRect.width * 0.2f - 40, npcWindowRect.height * 0.5f - 15, 80, 40 ), "Talk" ) )
+			{
+				bQuestGiving = false;
+				bShopping = false;
 				bConversation = true;
+			}
 
 			if ( GUI.Button ( new Rect ( npcWindowRect.width * 0.5f - 40, npcWindowRect.height * 0.5f - 15, 80, 40 ), "Shop" ) )
+			{
+				bConversation = false;
+				bQuestGiving = false;
 				bShopping = true;
+			}
 
 			if ( GUI.Button ( new Rect ( npcWindowRect.width * 0.8f - 40, npcWindowRect.height * 0.5f - 15, 80, 40 ), "Quest" ) )
+			{
+				bConversation = false;
+				bShopping = false;
 				bQuestGiving = true;
+			}
 		}
 	}
-	
+
+	//----------------------------------------------------------------------------------------------------------------------------
+	public void ShopWindow(int ID)
+	{
+		if ( GUI.Button ( new Rect ( shopWindowRect.width * 0.2f - 40, npcWindowRect.height * 0.8f - 15, 80, 40 ), "Buy" ) )
+		{
+
+		}
+
+		if ( GUI.Button ( new Rect ( shopWindowRect.width * 0.8f - 40, npcWindowRect.height * 0.8f - 15, 80, 40 ), "Sell" ) )
+		{
+			
+		}
+	}
+
+	//----------------------------------------------------------------------------------------------------------------------------
+	public void QuestWindow(int ID)
+	{
+		for ( int x = 0; x < numberOfQuests; x++ )
+		{
+			if ( GUI.Button ( new Rect (questWindowRect.width * 0.5f - 40, 30 + (x * 50), 80, 40 ), "Quest " + (x+1) ) )
+			{
+
+			}
+		}
+	}
+
+	//----------------------------------------------------------------------------------------------------------------------------
+	public void InventoryWindow(int ID)
+	{
+
+	}
+
+	//----------------------------------------------------------------------------------------------------------------------------
+	public void DescriptionWindow(int ID)
+	{
+
+	}
+
+	//----------------------------------------------------------------------------------------------------------------------------
+	public void SellWindow(int ID)
+	{
+
+	}
+
+	//----------------------------------------------------------------------------------------------------------------------------
 	// Update is called once per frame
 	void Update () {
 	
